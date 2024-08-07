d474471ce192385349b9903dc3d475be9c97bc9a
Tharmoth <tharmoth@gmail.com>
2024-08-07 17:28:54 -0600

    Added Blend Tilemap to Factory!

--- src/factory/FactoryTileMap.cs
+++ src/factory/FactoryTileMap.cs
@@ -20,6 +20,11 @@ public partial class FactoryTileMap : LayerTileMap
 	public const string Resource = "resource";
 	private const int Width = 600;
 	private const int Height = 600;
+	public const string SHADER_PARAM_MAP_DATA = "map_data";
+	public const string SHADER_PARAM_ENABLED = "enabled";
+	private static readonly Vector2I GRASS = new(0, 0);
+	private static readonly Vector2I OCEAN = new(4, 0);
+	private static readonly Vector2I DIRT = new(2, 0);
 	
 	/**************************************************************************
 	 * Private Variables                                                      *
@@ -143,6 +148,13 @@ public partial class FactoryTileMap : LayerTileMap
 	 **************************************************************************/
 	private void Initialize()
 	{
+		var noise = new FastNoiseLite()
+		{
+			NoiseType = FastNoiseLite.NoiseTypeEnum.SimplexSmooth,
+			Frequency = .1f,
+			Seed = (int) GD.Randi(),
+		};
+		
 		for (var y = 0; y < Width; y++)
 		{
 			for (var x = 0; x < Height; x++)
@@ -153,11 +165,18 @@ public partial class FactoryTileMap : LayerTileMap
 				
 				if (prov.Biome == Biome.Ocean)
 				{
-					SetCell(0, mapPosition, 2, new Vector2I(4, 0));
+					SetCell(0, mapPosition, 2, OCEAN);
 				}
 				else
 				{
-					SetCell(0, mapPosition, 2, new Vector2I(0, 0));
+					var noiseValue = Utils.NoiseNorm(noise, mapPosition) ;
+					var cellId = noiseValue switch
+					{
+						> 0.33 => GRASS,
+						_ => DIRT
+					};
+                    
+					SetCell(0, mapPosition, 2, cellId);
 					VisionTileMap.SetCell(0, mapPosition, 0, new Vector2I(10, 0));
 				}
 			}
@@ -166,13 +185,27 @@ public partial class FactoryTileMap : LayerTileMap
 		TilemapPropSpawner sparseGrassSpawner = new()
 		{
 			TargetTileMap = this, 
-			AtlasCoords = [new Vector2I(0, 0)],  
+			AtlasCoords = [GRASS, DIRT],  
 			Textures = [Database.Instance.GetTexture("Grass2")],
 			Threshold = 0.5f,
 			Density = 0.5f,
 			SizePixels = 24,
 		};
 		CallDeferred("add_sibling", sparseGrassSpawner);
+		
+		TilemapPropSpawner rockSpawner = new()
+		{
+			TargetTileMap = this, 
+			AtlasCoords = [DIRT],  
+			Textures = [Database.Instance.GetTexture("Rocks1"), Database.Instance.GetTexture("Rocks3")],
+			Threshold = 0.33f,
+			Density = 0.25f,
+			SizePixels = 24,
+			UseWind = false,
+		};
+		CallDeferred("add_sibling", rockSpawner);
+
+		Update();
 	}
 	
 	private void UpdateFogOfWar()
@@ -199,6 +232,7 @@ public partial class FactoryTileMap : LayerTileMap
 			.Map
 			.Provinces
 			.Where(province => province.Owner == "Player")
+			.Where(province => province.Biome != Biome.Ocean)
 			.Except(_unlockedProvinces)
 			.ToList()
 			.ForEach(newProvince =>
@@ -211,5 +245,31 @@ public partial class FactoryTileMap : LayerTileMap
 
 				_unlockedProvinces.Add(newProvince);
 			});
+	}
+
+	private void Update()
+	{
+		var rect = GetUsedRect();
+		var dataArray = new byte[rect.Size.X * rect.Size.Y];
+		int index = 0;
+		for (var y = rect.Position.Y; y < rect.End.Y; y++)
+		{
+			for (var x = rect.Position.X; x < rect.End.X; x++)
+			{
+				var cell = GetCellAtlasCoords(0, new Vector2I(x, y));
+				if (cell == OCEAN) dataArray[index] = (byte)4;
+				else if (cell == GRASS) dataArray[index] = (byte)2;
+				else if (cell == DIRT) dataArray[index] = (byte)1;
+				else dataArray[index] = (byte)0;
+				index++;
+			}
+		}
+
+		var img = Image.CreateFromData(rect.Size.X, rect.Size.Y, false, Image.Format.R8, dataArray);
+		var texture = ImageTexture.CreateFromImage(img);
+
+		var mat = (ShaderMaterial)Material;
+		mat.SetShaderParameter(SHADER_PARAM_MAP_DATA, texture);
+		mat.SetShaderParameter(SHADER_PARAM_ENABLED, true);
 	}
 }
